e21ef044fa81b75422ab37f1aaf8d02d626269fc
caperaven <caperaven@gmail.com>
2022-06-16 01:19:11 +0200

    initial

--- rust/src/features/color3.rs
+++ rust/src/features/color3.rs
@@ -27,7 +27,7 @@ extern "C" {
     #[wasm_bindgen(method, setter)]
     pub fn set_b(this: &Color3, val: i32);
 
-    #[wasm_bindgen(method, js_name = "toString")]
+    #[wasm_bindgen(method, js_name = toString)]
     pub fn to_string(this: &Color3) -> String;
 
     #[wasm_bindgen(js_name = Black, js_namespace = ["BABYLON", "Color3"])]
